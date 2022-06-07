688e14947f90c2a01d5ee987b33a89ebaaa7175f
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-06-07 11:01:14 -0300

    Continuação Refatorar JS inclusão controls.js e timer.js

---
+++ stage05exercicios/timerFocus/src/controls.js
@@ -0,0 +1,8 @@
+function resetControls() {
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    buttonStoping.classList.add('hide')
+    buttonSetting.classList.remove('hide')
+}
+
+export default resetControls
--- stage05exercicios/timerFocus/src/main.js
+++ stage05exercicios/timerFocus/src/main.js
@@ -1,7 +1,5 @@
-// let play = document.querySelector('.startCountdown')
-// play.classList.add('hide')
-
-// document.querySelector('.pauseCountdown').classList.remove('hide')
+import resetControls from "./controls.js"
+import "./timer.js"
 
 let buttonPlay = document.querySelector('.startCountdown')
 let buttonPause = document.querySelector('.pauseCountdown')
@@ -9,44 +7,10 @@ let buttonStoping = document.querySelector('.stopCountdown')
 let buttonSetting = document.querySelector('.settingCountdown')
 let buttonStopMusic = document.querySelector('.stopMusic')
 let buttonStartMusic = document.querySelector('.startMusic')
-let minutes 
 let minutesDisplay = document.querySelector('.minuts')
 let secondsDisplay = document.querySelector('.seconds')
-
-function resetControls() {
-    buttonPlay.classList.remove('hide')
-    buttonPause.classList.add('hide')
-    buttonStoping.classList.add('hide')
-    buttonSetting.classList.remove('hide')
-}
-
-function updateTimerDisplay(minutes, seconds) {
-    minutesDisplay.textContent = String(minutes).padStart(2, '0')
-    secondsDisplay.textContent = String(seconds).padStart(2, '0')
-}
-
-function countdown() {
-    setTimeout(() => {
-        let seconds = Number(secondsDisplay.textContent)
-        let minutes = Number(minutesDisplay.textContent)
-
-        updateTimerDisplay(minutes, 0)
-        
-        if (minutes <= 0) {
-            resetControls()
-            return
-        }
-
-        if( seconds <= 0) {
-            seconds = 2
-            --minutes
-        }
-
-        updateTimerDisplay(minutes, String(seconds - 1))
-
-        countdown()
-    }, 1000);
-}
+let minutes = Number(minutesDisplay.textContent)
+let timerTimeOut
 
 buttonPlay.addEventListener('click', function() {
     buttonPlay.classList.add('hide')
@@ -60,10 +24,12 @@ buttonPlay.addEventListener('click', function() {
 buttonPause.addEventListener('click', function() {
     buttonPause.classList.add('hide')
     buttonPlay.classList.remove('hide')
+    clearTimeout(timerTimeOut)
 })
 
 buttonStoping.addEventListener('click', function() {
     resetControls()
+    resetTimer()
 })
 
 buttonStopMusic.addEventListener('click', function() {
@@ -77,21 +43,22 @@ buttonStartMusic.addEventListener('click', function() {
 })
 
 buttonSetting.addEventListener('click', function() {
-        minutes = Number(prompt('Quantos minutos? (Escolha de 1 a 60)'))
-
-        if (minutes > 60) {
+        let newMinutes = Number(prompt('Quantos minutos? (Escolha de 1 a 60)'))
+    
+        if (newMinutes > 60) {
             alert("Por favor escolha um número válido de 1 a 60 minutos")
             
-        } else if (minutes <= 0) {
+        } else if (newMinutes <= 0) {
             alert("Por favor escolha um número válido de 1 a 60 minutos")
             
-        } else if (minutes === null) {
+        } else if (newMinutes === null) {
             alert("Por favor escolha um número válido de 1 a 60 minutos")
             
-        } else if (minutes === NaN) {
+        } else if (isNaN(newMinutes)) {
             alert("Por favor escolha um número válido de 1 a 60 minutos")
             
         } else {
+            minutes = newMinutes
             updateTimerDisplay(minutes, 0)
         }
 })
