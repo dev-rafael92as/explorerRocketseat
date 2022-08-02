c5b137ada19a78a152a925baf352f1ffb7bcf731
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-08-02 19:50:27 -0300

    [Stage08ExercicioAula]SQL e Node

---
+++ stage08/exercicioAula/src/controllers/UserController.js
@@ -0,0 +1,20 @@
+const AppError = require("../utils/AppError")
+
+const sqliteConnection = require("../database/sqlite")
+
+class UsersController {
+    async create(request, response) {
+        const { name, email, password } = request.body
+    
+        const database = await sqliteConnection();
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if(checkUserExists) {
+            throw new AppError("Este e-mail já está em uso.");
+        }
+
+        return response.status(201).json();
+    }
+}
+
+module.exports = UsersController
