682cb37beb4bf43e72fe6d4db56cde411b2139cc
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-09-05 16:47:22 -0300

    [Stage10ExercicioAula] Add JWT

--- stage10/exercicioAula/src/controllers/SessionsController.js
+++ stage10/exercicioAula/src/controllers/SessionsController.js
@@ -1,5 +1,8 @@
-const knex = require('../database/knex')
-const AppError = require('../utils/AppError')
+const knex = require('../database/knex');
+const AppError = require('../utils/AppError');
+const { compare } = require('bcryptjs');
+const { sign } = require('jsonwebtoken');
+const authConfig = require('../configs/auth');
 
 class SessionsContoller {
     async create(request, response) {
@@ -11,7 +14,19 @@ class SessionsContoller {
             throw new AppError("E-mail e/ou usuário incorreto", 401);
         }
 
-        return response.json( user );
+        const passwordMatched = await compare(password, user.password);
+
+        if(!passwordMatched) {
+            throw new AppError("E-mail e/ou usuário incorreto", 401);
+        }
+
+        const { secret, expiresIn } = authConfig.jwt;
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        });
+
+        return response.json({ user, token });
     }
 }
 
