0c006ee38d2840871c46ea41860368be9b7ecfd5
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-10-01 18:25:58 -0300

    [Stage10ExercicioAula] Add create account feat

--- stage10/exercicioAula/src/server.js
+++ stage10/exercicioAula/src/server.js
@@ -3,12 +3,14 @@ const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("../src/utils/AppError.js");
 const uploadConfig = require("./configs/upload")
 
+const cors = require("cors");
 const express = require("express");
 const routes = require("./routes");
 
 migrationsRun()
 
 const app = express();
+app.use(cors());
 app.use(express.json())
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
